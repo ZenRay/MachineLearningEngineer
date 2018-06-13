8ed0e460954360af3313605c0b389579b107be01
ZenRay <jake2011ren@gmail.com>
2018-06-13 16:06:43 +0800

    init the propress module

---
+++ P6_Capstone/tool/preprocess.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The preprocess module is a tool that collects all the data, preprocesses the 
+data. At the same time, utility the tool creates the features and the target. 
+
+"""
