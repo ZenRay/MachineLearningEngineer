55de2904d8e69eaab32d1a938de8d9eb32dd3486
ZenRay <jake2011ren@gmail.com>
2018-06-29 13:52:21 +0800

    feat($rmspe)  this is loss function that evaluate the value

--- P6_Capstone/tool/evaluation.py
+++ P6_Capstone/tool/evaluation.py
@@ -9,3 +9,24 @@ the evaluation module contains evaluation functions that like loss function and
 so on
 """
 
+def rmspe(y_true, y_predict):
+    """
+    Param:
+        (array) y_true - true labels
+        (array) y_predict - predict labels
+    Result:
+        (float) result - evaluation socre
+    """
+    if not(isinstance(y_predict, (np.ndarray, pd.core.series.Series)) and \
+        isinstance(y_true, (np.ndarray, pd.core.series.Series))):
+        raise "The type of y_true and y_predict must be ndarray, Series!"
+
+    if len(y_true) != len(y_predict):
+        raise "Length between y_predict and y_true isn't same!"
+
+    n = len(y_predict)
+    percent = (y_true - y_predict) / y_true
+
+    score = np.sqrt(np.power(percent, 2).sum() * 1/n)
+    
+    return "The rmspe score is %.4f .\n" % score
