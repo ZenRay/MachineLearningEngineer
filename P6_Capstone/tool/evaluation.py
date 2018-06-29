377878d000759334572008468aa70e5ebae0d584
ZenRay <jake2011ren@gmail.com>
2018-06-29 12:49:20 +0800

    feat($evaluation) init the tool functions about evaluation

---
+++ P6_Capstone/tool/evaluation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+"""
+the evaluation module contains evaluation functions that like loss function and
+so on
+"""
+
