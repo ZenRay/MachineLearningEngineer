dff7af6bb076647593abcacd6693f023143d5531
ZenRay <jake2011ren@gmail.com>
2018-07-04 21:54:53 +0800

    feat($get_result) convert the predict value, store the standard file to check the result

--- P6_Capstone/tool/evaluation.py
+++ P6_Capstone/tool/evaluation.py
@@ -42,3 +42,29 @@ def rmspe_xgb(y_true, y_predict):
     """
     score = rmspe(y_true, np.array(y_predict.get_label()))
     return "RMSPE", score
+
+def get_result(y_predict, data, path, file_name):
+    """
+    The function is used to convert the predict value into the exponential value.
+    Store the predict values, so that check the result.
+
+    Param:
+        y_predict - sequence value which store the predict value
+        (DataFrame) data - original data that it's dataframe
+        (string) path - the path is a directory that stores the result
+        (string) file_name - a csv file store information that is exported from
+                                the data to , so that check the prediction
+    Result:
+        (DataFrame) data - store the predict values
+    """
+    # duplicate the data
+    data = data.copy()
+
+    predict = np.expm1(y_predict)
+    data["Sales"] = predict
+
+    # sort the data by Id feature
+    data.sort_values("Id", inplace=True)
+    # export the data into csv file
+    data[["Id", "Sales"]].to_csv(path + file_name, index=False)
+    return data
